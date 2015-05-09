c33c503d1b88e3a89144957105ffba7b220017c0
David Cottrell <david_cottrell@scotiacapital.com>
2015-05-09 15:58:08 +0100

    Add kramdown, mathjax and archives.

---
+++ _posts/2015-05-10-something.md
@@ -0,0 +1,7 @@
+Here is some text. Inline $$\int f(x) \text{d} x$$
+
+$$ \mathbb{A} \equiv \mathcal{R} $$
+
+Did this work?
+
+
---
+++ setup.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+# install gem: https://rubygems.org/pages/download
+sudo gem install jekyll
+sudo gem install kramdown
